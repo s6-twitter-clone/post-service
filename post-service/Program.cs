36ae7bedcefaf4ec87023d81a0d74ef263b6ccc2
Jake <jakevanhout@outlook.com>
2022-05-12 17:54:49 +0200

    configure authentication and fix swagger reverse proxy issue
    - Added JWT authentication
    - Added configuration for firebase
    - Configured bearer authentication for swagger UI
    - Use X-Forwarded-Prefix header to fix request paths when running behind a reverse proxy
    - Added option to turn enable swagger UI in staging and production using an environment variable

--- post-service/Program.cs
+++ post-service/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using post_service.Data;
 using post_service.Interfaces;
 using post_service.Middlewares;
@@ -8,6 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddCors();
+
+
 builder.Services.AddControllers();
 builder.Services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
@@ -22,17 +27,81 @@ builder.Services.AddDbContext<DatabaseContext>(options =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(setup =>
+{
+    var jwtSecurityScheme = new OpenApiSecurityScheme
+    {
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Name = "Authentication",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+
+    setup.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+    setup.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { jwtSecurityScheme, Array.Empty<string>() }
+    });
+});
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(o =>
+{
+    o.Authority = builder.Configuration["Jwt:Authority"];
+
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+
+        ValidateIssuer = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+
+        ValidateLifetime = true
+    };
+
+});
+
+builder.Services.AddAuthorization();
+
 
 var app = builder.Build();
 
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || bool.Parse(Environment.GetEnvironmentVariable("ENABLE_SWAGGER") ?? bool.FalseString))
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(c =>
+    {
+        c.RouteTemplate = "/swagger/{documentName}/swagger.json";
+
+        c.PreSerializeFilters.Add((swaggerDoc, httpRequest) =>
+        {
+            if (!httpRequest.Headers.ContainsKey("X-Forwarded-Prefix")) return;
+
+            var serverUrl = "/" + httpRequest.Headers["X-Forwarded-Prefix"];
+
+            swaggerDoc.Servers = new List<OpenApiServer>()
+            {
+                new OpenApiServer { Url = serverUrl }
+            };
+        });
+    });
+
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("v1/swagger.json", "Post service");
+    });
 }
 
 
@@ -52,6 +121,7 @@ app.UseHttpsRedirection();
 
 app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
