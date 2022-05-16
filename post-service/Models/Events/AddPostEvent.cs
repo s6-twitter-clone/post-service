a1a5cb5343debc8c6623d7e5254f96e989879e6b
Jake <jakevanhout@outlook.com>
2022-05-16 19:53:04 +0200

    add event models

---
+++ post-service/Models/Events/AddPostEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace post_service.Models.Events;
+
+public class AddPostEvent
+{
+    public string Id { get; set; } = "";
+    public string Content { get; set; } = "";
+
+    public DateTime Posted { get; set; }
+
+    public string UserId { get; set; } = "";
+}
---
+++ post-service/Models/Events/AddUserEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace post_service.Models.Events;
+
+public class AddUserEvent
+{
+    public string Id { get; set; } = "";
+    public string DisplayName { get; set; } = "";
+}
